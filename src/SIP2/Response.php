22b799d20ca10e05f2ed49c9e82a082bf3bf9e9e
Josef Moravec <josef.moravec@gmail.com>
2018-02-09 12:07:33 +0100

    Remove var dump

--- src/SIP2/Response.php
+++ src/SIP2/Response.php
@@ -47,10 +47,8 @@ class Response
 
     protected function parse()
     {
-        var_dump($this->type);
         $parseFunctionName = "parse" . $this->type["parse"] . "Response";
         $parsed = $this->connection->$parseFunctionName($this->message);
-        var_dump($parsed);
         $this->fixed = $parsed["fixed"];
         $this->variable = $parsed["variable"];
     }
