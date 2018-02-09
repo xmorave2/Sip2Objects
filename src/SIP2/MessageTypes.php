26b2c2a6cf284f236e150f25278760737adf7d03
Josef Moravec <josef.moravec@gmail.com>
2018-02-09 10:11:12 +0100

    Make message types constants

--- src/SIP2/MessageTypes.php
+++ src/SIP2/MessageTypes.php
@@ -4,36 +4,36 @@ namespace SIP2;
 
 class MessageTypes
 {
-    public static PATRON_STATUS =
+    const PATRON_STATUS =
         ["msg" => "PatronStatus", "parse" => "PatronStatus"];
-    public static CHECKOUT =
+    const CHECKOUT =
         ["msg" => "Checkout", "parse" => "Checkout"];
-    public static CHECKIN =
+    const CHECKIN =
         ["msg" => "Checkin", "parse" => "Checkin"];
-    public static BLOCK_PATRON =
+    const BLOCK_PATRON =
         ]"msg" => "BlockPatron", "parse" => ""];
-    public static SC_STATUS =
+    const SC_STATUS =
         ["msg" => "SCStatus", "parse" => "ACSStatus"];
-    public static REQUEST_ACS_RESEND =
+    const REQUEST_ACS_RESEND =
         ["msg" => "RequestACSResend", "parse" => "ACSStatus"];
-    public static LOGIN =
+    const LOGIN =
         ["msg" => "Login", "parse" => "Login"];
-    public static PATRON_INFORMATION =
+    const PATRON_INFORMATION =
         ["msg" => "PatronInformation", "parse" => "PatronInfo"]; 
-    public static END_PATRON_SESSION =
+    const END_PATRON_SESSION =
         ["msg" => "EndPatronSession", "parse" => "EndSession"];
-    public static FEE_PAID =
+    const FEE_PAID =
         ["msg" => "FeePaid", "parse" => "FeePaid"];
-    public static ITEM_INFORMATION =
+    const ITEM_INFORMATION =
         ["msg" => "ItemInformation", "parse" => "ItemInfo"];
-    public static ITEM_STATUS =
+    const ITEM_STATUS =
         ["msg" => "ItemStatus", "parse" => "ItemStatus"];
-    public static PATRON_ENABLE =
+    const PATRON_ENABLE =
         ["msg" => "PatronEnable", "parse" => "PatronEnable"];
-    public static HOLD =
+    const HOLD =
         ["msg" => "Hold", "parse" => "Hold"];
-    public static RENEW =
+    const RENEW =
         ["msg" => "Renew", "parse" => "Renew"];
-    public static RENEW_ALL =
+    const RENEW_ALL =
         ["msg" => "RenewAll", "parse" => "RenewAll"];
 }
